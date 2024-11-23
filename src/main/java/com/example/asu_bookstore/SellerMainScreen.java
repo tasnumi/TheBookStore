08105d8aef283e54c8066a038646bbee77a73005
Basim4 <114629174+Basim4@users.noreply.github.com>
2024-11-23 06:09:59 -0700

    SellerMainScreen Comments

--- src/main/java/com/example/asu_bookstore/SellerMainScreen.java
+++ src/main/java/com/example/asu_bookstore/SellerMainScreen.java
@@ -107,10 +107,10 @@ public class SellerMainScreen extends BorderPane {
         text.setPrefWidth(15);
         text.setPrefHeight(15);
 
+        // This VBox holds and displays all the books in the database on the right side of the screen
         VBox printData = new VBox(10);
 
-        leftContainer.getChildren().addAll(label1, checkbox1, checkbox2, checkbox3, checkbox4, label2, combobox, label3, text,label4, title);
-
+        // This button displays the price the user is trying to sell based on the condition and genre of the book
         Button listBook = new Button("List Book");
         VBox rightContainer = new VBox(10);
         rightContainer.setStyle("-fx-background-color: #be96c4; -fx-border-color: #8273da; -fx-border-width: 0.5; -fx-padding: 20");
@@ -125,10 +125,12 @@ public class SellerMainScreen extends BorderPane {
         checkbox4.setOnAction(e -> HandleOptions(checkbox1, checkbox2, checkbox3, checkbox4, text, combobox, printData,title, bookDatabase));
         combobox.setOnAction(e -> HandleOptions(checkbox1, checkbox2, checkbox3, checkbox4, text, combobox, printData,title, bookDatabase));
 
+        // set the behavior of the combobox
         listBook.setOnAction(e -> {
             HandleOptions(checkbox1, checkbox2, checkbox3, checkbox4, text, combobox, printData,title, bookDatabase);
         });
 
+        // add the right side of the screen into the scene
         rightContainer.getChildren().addAll(listBook, printData,r);
 
         // The buyer sun image is loaded from the program's resources folder and is placed into the scene
@@ -150,6 +152,7 @@ public class SellerMainScreen extends BorderPane {
         Button logOut = new Button("Log Out");
         Button purchase = new Button("Sell");
 
+        // this defines the logout button's behavior.
         logOut.setOnAction(e -> {
             // "refreshing" the screen for the next user to have a blank slate
             t.selectToggle(null);
@@ -161,9 +164,12 @@ public class SellerMainScreen extends BorderPane {
             control.switchScreen("");
         });
 
+        // this defines the purchase button's behavior
         purchase.setOnAction(e -> {
-
+            // if any of the required data feilds that the user must enter are not given, then the program will NOT enter
+            // the final seller confirmation screen.
             if (!(text.getText().trim().isEmpty() || title.getText().trim().isEmpty() || combobox.getSelectionModel() == null || t.getSelectedToggle() == null)) {
+                // send the information to the SellerConfirmationScreen and write to seller history textfile
                 control.getSellerConfimScreen().constructSaleInfo(this);
 
                 // "refreshing" the screen for the next user to have a blank slate
@@ -172,12 +178,15 @@ public class SellerMainScreen extends BorderPane {
                 text.clear();
                 title.clear();
 
-                control.switchScreen("sellerConfirm");
+                control.switchScreen("sellerConfirm"); // switch the screen to the seller finalization/confrim screen
             }
 
         });
 
-        // The behavior for this screen's buttons are defined here
+        // all the nodes are added to the screen
+        leftContainer.getChildren().addAll(label1, checkbox1, checkbox2, checkbox3, checkbox4, label2, combobox, label3, text,label4, title);
+
+        // The placement of this screen's buttons are defined here
         logAndSellButtons.getChildren().addAll(logOut, purchase);
 
         // This adds the main components to the screen
@@ -186,13 +195,16 @@ public class SellerMainScreen extends BorderPane {
         this.setBottom(logAndSellButtons);
     }
 
+    // This HandleOptions method processes all the information given by the user in the input TextFeilds, RadioButtons,
+    // and ComboBoxes. This method ensures that all parameters are given and calculates a system-generated price for the
+    // user's book. This price is shown when the user clicks the "List Book" button. The price generated here also is sent
+    // to the SellerConfirmationScreen object that is displayed when the user finalizes their sale.
     public void HandleOptions(RadioButton checkbox1, RadioButton checkbox2, RadioButton checkbox3, RadioButton checkbox4, TextField text, ComboBox<String> combobox, VBox printData, TextField title,  ArrayList<String> bookDatabase) {
         printData.getChildren().clear();
         ArrayList<String> f = new ArrayList<>();
 
+        // checks if the user provided a genre to the system.
         String selectedGenre = (String) combobox.getValue();
-
-        // if (checkbox1.isSelected() || checkbox2.isSelected() || checkbox3.isSelected() || checkbox4.isSelected() && !selectedGenre.isEmpty() && !text.getText().isEmpty()) {
         if (selectedGenre == null || selectedGenre.isEmpty()) {
             Label error = new Label("Please enter a genre");
             error.setStyle(" -fx-font: 15 Arial;-fx-font-weight: bold; -fx-text-fill: #FFFFFF; -fx-underline: true");
@@ -200,6 +212,7 @@ public class SellerMainScreen extends BorderPane {
             printData.getChildren().add(error);
             return;
         }
+        // checks if the user provided a price to the system.
         if (text.getText() == null || text.getText().isEmpty()) {
             Label error = new Label("Please input a price");
             error.setStyle(" -fx-font: 15 Arial;-fx-font-weight: bold; -fx-text-fill: #FFFFFF; -fx-underline: true");
@@ -208,6 +221,7 @@ public class SellerMainScreen extends BorderPane {
             return;
         }
 
+        // Check to make sure that the user matches the book condition that the system has in its database.
         for (int i = 0; i < bookDatabase.size(); i++) {
             String book = bookDatabase.get(i);
             boolean matches = false;
@@ -225,17 +239,19 @@ public class SellerMainScreen extends BorderPane {
             }
             boolean genreCheck = book.contains("Genre: " + selectedGenre);
 
-            if (matches && genreCheck) {
+            if (matches && genreCheck) { // if there is a match, then add it to arrayList
                 f.add(book);
             }
         }
 
+        // collect the title of each book in the database for comparisons with the user's given title (in the next code block)
         String t = null;
         for (int i = 0; i < f.size(); i++) {
             String book = f.get(i);
             t = book.split("Title: ")[1].split(" \\| ")[0];
-
         }
+
+        // compares the database title to the user's title
         if (!Objects.equals(t, title.getText())) {
         Label label = new Label("This Book is not in the Database");
             label.setStyle(" -fx-font: 15 Arial;-fx-font-weight: bold; -fx-text-fill: #FFFFFF; -fx-underline: true");
@@ -243,34 +259,43 @@ public class SellerMainScreen extends BorderPane {
         return;
         }
 
-        try {
+        try { // if the input value is not an integer, the program an exception and the user can try again
             int inputVal = Integer.parseInt(text.getText());
+            // each book condition has a different rate. The better the condition, the more expensive the price will be.
+            // this statement checks for and calculates the price for "Used" books
             if (checkbox1.isSelected() && (selectedGenre.equals("Natural Science") || selectedGenre.equals("Math") || selectedGenre.equals("Computer") || selectedGenre.equals("English") || selectedGenre.equals("Language") || selectedGenre.equals("Others")) && inputVal > 0) {
                 double price = inputVal * 0.70;
                 String p = String.format("This is your price $%.2f", price);
                 Label label = new Label(p);
                 label.setStyle(" -fx-font: 15 Arial;-fx-font-weight: bold; -fx-text-fill: #FFFFFF; -fx-underline: true");
                 printData.getChildren().add(label);
-            } else if (checkbox2.isSelected() && (selectedGenre.equals("Natural Science") || selectedGenre.equals("Math") || selectedGenre.equals("Computer") || selectedGenre.equals("English") || selectedGenre.equals("Language") || selectedGenre.equals("Others")) && inputVal > 0) {
+            }
+            // this statement checks for and calculates the price for "Like New" books
+            else if (checkbox2.isSelected() && (selectedGenre.equals("Natural Science") || selectedGenre.equals("Math") || selectedGenre.equals("Computer") || selectedGenre.equals("English") || selectedGenre.equals("Language") || selectedGenre.equals("Others")) && inputVal > 0) {
                 double price = inputVal * 0.90;
                 String p = String.format("This is your price $%.2f", price);
                 Label label = new Label(p);
                 label.setStyle(" -fx-font: 15 Arial;-fx-font-weight: bold; -fx-text-fill: #FFFFFF; -fx-underline: true");
                 printData.getChildren().add(label);
-            } else if (checkbox3.isSelected() && (selectedGenre.equals("Natural Science") || selectedGenre.equals("Math") || selectedGenre.equals("Computer") || selectedGenre.equals("English") || selectedGenre.equals("Language") || selectedGenre.equals("Others")) & inputVal > 0) {
+            }
+            // this statement checks for and calculates the price for "Moderately Used" books
+            else if (checkbox3.isSelected() && (selectedGenre.equals("Natural Science") || selectedGenre.equals("Math") || selectedGenre.equals("Computer") || selectedGenre.equals("English") || selectedGenre.equals("Language") || selectedGenre.equals("Others")) & inputVal > 0) {
                 double price = inputVal * 0.85;
                 String p = String.format("This is your price $%.2f", price);
                 Label label = new Label(p);
                 label.setStyle(" -fx-font: 15 Arial;-fx-font-weight: bold; -fx-text-fill: #FFFFFF; -fx-underline: true");
                 printData.getChildren().add(label);
-            } else if (checkbox4.isSelected() && (selectedGenre.equals("Natural Science") || selectedGenre.equals("Math") || selectedGenre.equals("Computer") || selectedGenre.equals("English") || selectedGenre.equals("Language") || selectedGenre.equals("Others")) & inputVal > 0) {
+            }
+            // this statement checks for and calculates the price for "Heavily Used" books
+            else if (checkbox4.isSelected() && (selectedGenre.equals("Natural Science") || selectedGenre.equals("Math") || selectedGenre.equals("Computer") || selectedGenre.equals("English") || selectedGenre.equals("Language") || selectedGenre.equals("Others")) & inputVal > 0) {
                 double price = inputVal * 0.50;
                 String p = String.format("This is your price $%.2f", price);
                 Label label = new Label(p);
                 label.setStyle(" -fx-font: 15 Arial;-fx-font-weight: bold; -fx-text-fill: #FFFFFF; -fx-underline: true");
                 printData.getChildren().add(label);
             }
-        } catch (Exception e) {
+        }
+        catch (Exception e) { // if an error occurs with the integer price parsing, throw an exception to the user.
             throw new RuntimeException(e);
         }
     }
